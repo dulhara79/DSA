b35f94e3086fdf7909202a25ac6e6a0c99fe2aae
dulhara kaushalya <dulhara.kaushalya79@gmail.com>
2024-10-09 12:32:36 +0530

    Create bubbleSort.py

---
+++ Algirithms/bubbleSort.py
@@ -0,0 +1,25 @@
+'''
+BUBBLESORT(A)
+    1. for i = 1 to A.length - 1
+    2.    for j = A.length downto i + 1
+    3.        if A[j] < A[j - 1]
+    4.            exchange A[j] with A[j - 1]
+'''
+
+arr = []
+for v in range(5):
+    arr.append(int(input("Enter number: ")))
+
+print("Unsorted array: ", arr)
+
+def bubbleSort(arr):
+    n = len(arr)
+    for i in range(n - 1):
+        for j in range(1, n - i):
+            if arr[j] < arr[j - 1]:
+                arr[j], arr[j - 1] = arr[j - 1], arr[j]
+
+bubbleSort(arr)
+
+print("Sorted array: ", arr)
+
